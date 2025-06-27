3607380c5805cba1c89216e700b379bb30eebb26
souph1s <sophiamperez@hotmail.com>
2025-06-27 08:54:09 -0300

    feat: finish project configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,38 @@
-import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
-import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.register(fastifyCors)
+app.register(fastifyCors);
 
 app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'NLW Connect',
-      version: '0.0.1',
-    },
-  },
-  transform: jsonSchemaTransform,
-})
-
-app.register(fastifySwaggerUi, {
-  routePrefix: '/docs',
-})
-
-app.register(subscribeToEventRoute)
-
-app.listen({ port: 3333 }).then(() => {
-  console.log("Http Server Running")
+	openapi: {
+		info: {
+			title: "NLW Connect",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
 });
 
+app.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+
+app.register(subscribeToEventRoute);
+
+app.listen({ port: 3333 }).then(() => {
+	console.log("Http Server Running");
+});
